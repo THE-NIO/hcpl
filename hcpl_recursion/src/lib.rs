f19440d22810809842996a7367c59dfd7cab3221
Magnus Hegdahl <magnus.hegdahl@gmail.com>
2023-05-15 23:53:53 +0200

    simplify let_rec_mut, now list only mut "captures"

--- hcpl_recursion/src/lib.rs
+++ hcpl_recursion/src/lib.rs
@@ -24,38 +24,28 @@ macro_rules! _recursion__let_rec {
 
 #[macro_export]
 macro_rules! _recursion__let_rec_mut__impl {
-    ([$($cap_id:ident: $cap_ty:ty,)*] [$($mut_cap_id:ident: $mut_cap_ty:ty,)*] [$head:ident: $head_type:ty, $($tail:tt)*] $($rest:tt)*) => {
-        hcpl_recursion::_recursion__let_rec_mut__impl!([$($cap_id: $cap_ty,)* $head: $head_type,] [$($mut_cap_id: $mut_cap_ty,)*] [$($tail)*] $($rest)*);
-    };
-    ([$($cap_id:ident: $cap_ty:ty,)*] [$($mut_cap_id:ident: $mut_cap_ty:ty,)*] [mut $head:ident: $head_type:ty, $($tail:tt)*] $($rest:tt)*) => {
-        hcpl_recursion::_recursion__let_rec_mut__impl!([$($cap_id: $cap_ty,)*] [$($mut_cap_id: $mut_cap_ty,)* $head: $head_type,] [$($tail)*] $($rest)*);
-    };
-    ([$($cap_id:ident: $cap_ty:ty,)*] [$($mut_cap_id:ident: $mut_cap_ty:ty,)*] [$head:ident: $head_type:ty] $($rest:tt)*) => {
-        hcpl_recursion::_recursion__let_rec_mut__impl!([$($cap_id: $cap_ty,)* $head: $head_type,] [$($mut_cap_id: $mut_cap_ty,)*] [] $($rest)*);
-    };
-    ([$($cap_id:ident: $cap_ty:ty,)*] [$($mut_cap_id:ident: $mut_cap_ty:ty,)*] [mut $head:ident: $head_type:ty] $($rest:tt)*) => {
-        hcpl_recursion::_recursion__let_rec_mut__impl!([$($cap_id: $cap_ty,)*] [$($mut_cap_id: $mut_cap_ty,)* $head: $head_type,] [] $($rest)*);
-    };
-    ([$($cap_id:ident: $cap_ty:ty,)*] [$($mut_cap_id:ident: $mut_cap_ty:ty,)*] [] $f:ident = |$($arg_id:ident: $arg_ty:ty),*| -> $ret:ty $body:block $dol:tt) => {
-        fn call($($cap_id: &$cap_ty,)* $($mut_cap_id: &mut $mut_cap_ty,)* $($arg_id: $arg_ty),*) -> $ret {
+    ($f:ident = [ $($cap_id:ident: $cap_ty:ty),* $(,)? ] |$($arg_id:ident: $arg_ty:ty),*| -> $ret:ty $body:block $dol:tt) => {
+        hcpl_recursion::let_rec!(without_cap = |$($cap_id: &mut $cap_ty,)* $($arg_id: $arg_ty),*| -> $ret {
             macro_rules! $f {
                 ($dol ($dol inner_args:tt),*) => {
-                    call($($cap_id,)* $($mut_cap_id,)* $dol ($dol inner_args),*)
-                };
+                    without_cap($($cap_id,)* $dol ($dol inner_args,)*)
+                }
             }
             $body
+        });
+        macro_rules! $f {
+            ($dol ($dol inner_args:tt),*) => {
+                without_cap($(&mut $cap_id,)* $dol ($dol inner_args,)*)
+            }
         }
-        let mut $f = |$($arg_id: $arg_ty),*| {
-            call($(&$cap_id,)* $(&mut $mut_cap_id,)* $($arg_id),*);
-        };
-    }
+    };
 }
 
 #[macro_export]
 macro_rules! _recursion__let_rec_mut {
-    ($f:ident = [$($captures:tt)*] |$($arg_id:ident: $arg_ty:ty),*| -> $ret:ty $body:block) => {
-        hcpl_recursion::_recursion__let_rec_mut__impl!([] [] [$($captures)*] $f = |$($arg_id: $arg_ty),*| -> $ret $body $);
-    };
+    ($($args:tt)*) => {
+        hcpl_recursion::_recursion__let_rec_mut__impl!($($args)* $);
+    }
 }
 
 pub use crate::{
